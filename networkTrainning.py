45f41a173ae6c5fa9833516cf48bced3aa35ac7b
Javier Galan <javier.galan@unizar.es>
2022-01-17 19:40:31 +0100

    Adding networkTrainning script

--- networkTrainning.py
+++ networkTrainning.py
@@ -1,60 +1,139 @@
-import os, sys
+import os, sys, time
 import ROOT
+import numpy as np
 
-netSize = 30
-nodeDistance = 1.5
+import SOFM
 
-positions = []
+Ro = SOFM.netSize/4.
+Rf = 1
 
-row = []
-#
-#row.append([0.0, 1.0])
-#row.append([0.1, 1.0])
-#row.append([0.2, 1.0])
-#row.append([0.3, 1.0])
-#
-#positions.append(row)
-#
-#row = []
-#
-#row.append([0.0, 2.0])
-#row.append([0.1, 2.0])
-#row.append([0.2, 2.0])
-#row.append([0.3, 2.0])
+alpha_o = 0.99
+alpha_f = 0.01
 
-#positions.append(row)
+Iterations = 10
 
-## We define the coordinates of node n,m for an hexagonal network
-## n identifies the row (y-direction)
+maxSamples = 500
 
-## n,m are the node coordinates
-## D is the hexagon dimension. Number of neighbours
-## netDimension the number of nodes
-def get_list_of_nodes ( n, m, D, netDimension = 30 ):
-    nodes = []
+### reading dataset and initializing network
+dataset = np.loadtxt("data/trainSet.txt")
+nVars = len(dataset[0]) - 1
+nData = len(dataset)
 
-    ## Top-hexagon
-    for j in range(D+1):
-        Np = 2*D+1-j
-        Y = m + j
-        for i in range(Np):
-            X = n - D + i
-            node = [X%netDimension, Y%netDimension]
-            if( X != n or Y != m ):
-                nodes.append(node)
+print ("vars: " + str( nVars ) )
+print ("data: " + str( nData ) )
 
-    ## Bottom-hexagon
-    for j in range(1,D+1):
-        Np = 2*D+1-j
-        Y = m - j
-        for i in range(Np):
-            X = n - D + j + i
-            node = [X%netDimension, Y%netDimension]
-            if( X != n or Y != m ):
-                nodes.append(node)
+## We initialize the network with mean and sigmas from dataset
+means = []
+for n in range(1,nVars+1):
+    means.append(np.mean(dataset[:,n]))
+means = np.array(means)
 
-    return nodes
+sigmas = []
+for n in range(1,nVars+1):
+    sigmas.append(np.std(dataset[:,n]))
+sigmas = np.array(sigmas)
 
-nodes = get_list_of_nodes( 9, 9, 2, 10 )
+def initialize_network( ):
+    nVars = len(means)
 
-print (nodes)
+    rootRnd = ROOT.TRandom3()
+    rootRnd.SetSeed( int (time.time()) ) 
+
+    network = []
+    for i in range(SOFM.netSize): 
+        netRow = []
+        for j in range( SOFM.netSize):
+            vector = []
+            for n in range(nVars):
+                value = rootRnd.Gaus( means[n], sigmas[n] )
+                vector.append( value )
+            netRow.append( np.array(vector) )
+        network.append(netRow)
+    return network
+
+wNetwork = initialize_network(  )
+
+## Winning neuron of first element in the dataset
+## print ( "Testing winning neuron identification" )
+## print ( SOFM.get_winning_neuron( wNetwork, dataset[0][1:8] ) )
+
+def R( t ):
+    return Ro + float(t)*(Rf-Ro)/Iterations
+
+def h( dist, t ):
+    return ROOT.TMath.Exp(-dist/R(t))
+
+def alpha(t):
+    return alpha_o + float(t)*(alpha_f-alpha_o)/Iterations
+
+def update_neuron( n, m, dist, vector, t ):
+    wNetwork[n][m] = wNetwork[n][m] + alpha(t) * h( dist, t) * ( vector - wNetwork[n][m] );
+
+######################################################
+### Above code (together with SOFM.py) is given to the student
+###
+### See student.py
+###
+### The output file must contain the  weights vector of each network cell.
+### Proposed format:
+###
+### 0 0 w1 .... wN
+### 0 1 w1 .... wN
+### ...
+### 29 29 w1 .... wN
+###
+### The coming code is work for the student.
+### So this is a possible solution to the exercise. 
+### This must be removed from the lecture.
+###
+
+def update_region( vector, t ):
+
+    wN = SOFM.get_winning_neuron( wNetwork, vector ) 
+
+    lNodes = SOFM.get_list_of_nodes ( wN[0], wN[1], int( 2*R(t) ) )
+
+    for node in lNodes:
+        d = SOFM.node_distance( wN[0]-node[0], wN[1]-node[1] )
+        update_neuron( node[0], node[1], d, vector, t )
+
+    return 0
+
+##### Advice: For each method you write-down write few lines for testing input 
+##### and output results
+## We test the update of neuron (n,m) using first element in the dataset
+## print ( "Testing update neuron " )
+## print ( "Input vector : " )
+## print ( dataset[0][1:8] )
+## print ( "Neuron before : ")
+## print ( wNetwork[6][8] )
+## print ( update_neuron( 6, 8, 1.5, dataset[0][1:8], 5 ) )
+## print ( "Neuron after : ")
+## print ( wNetwork[6][8] )
+
+
+def networkTraining( ):
+
+    for iteration in range(Iterations):
+        counter = 0
+        print ("Iteration " + str( iteration ) + "\n" )
+        for v in dataset:
+            counter = counter + 1
+            update_region( v[1:8], iteration )
+            if counter % 1000 == 0:
+                print ("counter: " + str(counter) )
+            if counter > maxSamples:        
+                print( "Reseting at sample : " + str(counter) )
+                break 
+
+    f = open("data/network.out", "wt" )
+    for n in range(SOFM.netSize):
+        for m in range(SOFM.netSize):
+            f.write(str(n)+"\t"+str(m) )
+            for l in range(len(wNetwork[n][m]) ):
+                f.write( "\t" + str(wNetwork[n][m][l]) )
+            f.write("\n")
+    f.close()
+###
+
+networkTraining()
